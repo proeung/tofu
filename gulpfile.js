869d5a2317015beaf7c58543de910ebadeb66a94
proeung <putra.roeung@gmail.com>
2018-05-03 11:28:32 -0400

    Updated package.json and moved compiled css and js files to dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,64 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
-var minifycss = require('gulp-minify-css');
 var cleanCSS = require('gulp-clean-css');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
+var babel = require('gulp-babel');
+var sassdoc = require('sassdoc');
 
 var paths = {
-    styles: ['./css/scss/style.scss', './css/scss/ie.scss']
+    styles: [
+        './src/scss/style.scss', 
+        './src/scss/ie.scss'
+    ]
 }
 
-gulp.task('css', function ()
-{
+gulp.task('css', function () {
     gulp.src(paths.styles)
         .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(minifycss())
         .pipe(autoprefixer({ browsers: ['last 3 versions'] }))
-        .pipe(gulp.dest('css'))
-        .pipe(notify({message: 'SCSS Compiled!'}));
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(cleanCSS({}))
+        .pipe(rename(function (path) {
+             path.basename += ".min";
+        }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(notify({ message: 'SCSS Compiled!' }));
 });
 
-gulp.task('js', function ()
-{
+gulp.task('js', function () {
 
-    gulp.src('./js/vendor/plugins/*.js')
+    gulp.src('./src/js/vendor/plugins/*.js')
         .pipe(plumber())
         .pipe(concat('plugins.min.js'))
         //.pipe(uglify())
-        .pipe(gulp.dest('./js/vendor'))
+        .pipe(gulp.dest('dist/js/vendor'))
         .pipe(notify({message: 'Plugins Compiled and Minified!'}));
 
-    gulp.src('./js/functionality.js')
+    gulp.src('./src/js/functionality.js')
         .pipe(plumber())
+        .pipe(gulp.dest('dist/js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('js'))
-        .pipe(notify({message: 'JS Compiled!'}));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(notify({message: 'Compiled <%= file.relative %>!'}));
 });
 
-gulp.task('watch', function ()
-{
-    gulp.watch('./css/scss/*.scss', ['css']);
-    gulp.watch('./css/scss/**/*.scss', ['css']);
-    gulp.watch('./js/vendor/plugins/*.js', ['js']);
-    gulp.watch('./js/functionality.js', ['js']);
+gulp.task('watch', function () {
+    gulp.watch('./src/scss/*.scss', ['css']);
+    gulp.watch('./src/scss/**/*.scss', ['css']);
+    gulp.watch('./src/js/vendor/plugins/*.js', ['js']);
+    gulp.watch('./src/js/functionality.js', ['js']);
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('compile', ['css', 'js']);
---
+++ src/js/functionality.js
@@ -0,0 +1,109 @@
+(function ($, Drupal, drupalSettings, window, document, undefined) {
+
+    /********************
+     ** Global Variables
+     ********************/
+
+    var mq = jQuery('#js-mediaQueryReference')[0],
+        mqSupport = Modernizr.mq('only all'),
+        prevDevice = '',
+        device = '',
+        $body = $("body"),
+        $page = $("#page"),
+        activeClass = 'is-active',
+        stickyClass = 'is-sticky',
+        staticClass = 'is-static',
+        $mobileBtn = $('.menu-toggle');
+
+    /********************
+    ** Blazy
+    ********************/
+
+    function initBLazy() {
+      var bLazy = new Blazy({
+        offset: 0,
+        success: function(element, context) {
+            // console.log('Blazy - Success');
+            setTimeout(function(){
+            }, 100);
+        },
+        error: function(element, context) {
+            // console.log('Blazy - Error');
+        }
+      });
+    }
+
+
+    //Function to reset any styles that may have been changed on screen resize
+    function resetStyles() {
+        if (device == 'desktop') {
+            $body.removeClass('mobile');
+            // console.log('desktop');
+        }
+        else {
+            $body.addClass('mobile');
+            // console.log('mobile');
+        }
+    }
+
+    //Create an event checker function that grabs the current value of the after pseudo class of the #js-mediaquery-reference <div>
+    //Based on: http://seesparkbox.com/demos/css-content-check/index.html
+    function checkMediaQuery() {
+        if (mqSupport) {
+            prevDevice = device;
+            device = window.getComputedStyle(mq, ':after').getPropertyValue('content').replace(/"/g, '');
+            resetStyles();
+        }
+        else {
+            device = 'desktop';
+        }
+    }
+
+    function checkDevice() {
+        //Set up event listeners tied to media queries
+        mq.addEventListener('webkitTransitionEnd', checkMediaQuery, true);
+        mq.addEventListener('MSTransitionEnd', checkMediaQuery, true);
+        mq.addEventListener('oTransitionEnd', checkMediaQuery, true);
+        mq.addEventListener('transitionend', checkMediaQuery, true);
+        checkMediaQuery();
+    }
+
+    /********************
+     ** jQuery Window Load
+     ********************/
+
+    jQuery(window).on('load', function(){ });
+
+    /********************
+     ** jQuery Window Scroll
+     ********************/
+
+    jQuery(window).on('scroll', function(){ });
+
+    /********************
+     ** jQuery DOM Ready
+     ********************/
+
+    jQuery(document).ready(function () {
+        //If the browser supports media queries
+        if (mqSupport) {
+            //Check what device we're on
+            checkDevice();
+        }
+    });
+
+    //To understand behaviors, see https://drupal.org/node/756722#behaviors
+    Drupal.behaviors.tofu_behavior = {
+        attach: function(context, settings) {
+            initBLazy();
+    
+            $('.view').bind("ajaxStart", function(){
+                // console.log("Ajax START!");
+            }).bind("ajaxSuccess", function(){
+                initBLazy();
+                // console.log("Ajax Success!");
+            });
+        }
+    };
+
+})(jQuery, Drupal, drupalSettings, this, this.document);
